51f780a97e42cdfee11e3c21dbe166a4827af33a
Georgesui <gdimov445@gmail.com>
2022-05-08 18:04:18 +0300

    my commit

---
+++ style.js
@@ -0,0 +1,32 @@
+console.log('#4-----------')
+var name = 'Vic 2';
+var user = {
+	age: 5,
+	name: 'Vic',
+	getName: function () {
+		return this.name;
+	}
+};
+
+const getName = user.getName.bind(user);
+console.log(getName())
+// const getName = user.getName.call(user);
+// console.log(getName)
+// const getName = user.getName.apply(user);
+// console.log(getName)
+
+console.log('#5-----------')
+
+const user1 = {
+	age: 5,
+	name: 'Vic',
+	getNameSecond: function () {
+		return this.name;
+	}
+};
+const user2 = {
+	name: 'Arni',
+}
+
+const getNameSecond = user1.getNameSecond.apply(user2);
+console.log(getNameSecond)
